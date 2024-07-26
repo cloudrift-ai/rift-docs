2b8ac3ec8dbba0823f782822f2ae3a773aa43f7e
yash-asvi <nistern69@gmail.com>
2024-07-26 19:45:23 +0530

    Fixed redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Home() {
     React.useEffect(() => {
-        window.location.href = '/getting_started';
+        window.location.href = 'docs/getting_started';
     }, []);
     return null;
 }
