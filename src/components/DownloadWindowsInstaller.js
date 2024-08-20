49346ad99e3d30e8b6c2e85fccb8733dbbe1da44
Dmitry Trifonov <slonegg@gmail.com>
2024-08-20 11:47:14 -0700

    add new component for Windows installer link

---
+++ src/components/DownloadWindowsInstaller.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import Link from '@docusaurus/Link';
+
+const DownloadWindowsInstaller = () => {
+    const [url, setUrl] = useState('');
+
+    useEffect(() => {
+        fetch('https://faircompute.com:8000/internal/release/fair-desktop-full/stable/windows-x86_64/latest/url')
+            .then((response) => response.text())
+            .then((url) => setUrl(url));
+    }, []);
+
+    if (!url) {
+        return <span>Loading...</span>;
+    }
+
+    return <Link to={url}>Download Windows Installer</Link>;
+};
+
+export default DownloadWindowsInstaller;
