16e202224c04dfcfdf82b3e4d373b36d288a297d
Dmitry Trifonov <slonegg@gmail.com>
2024-10-22 12:03:51 -0700

    fix Windows installer link

--- src/components/DownloadWindowsInstaller.js
+++ src/components/DownloadWindowsInstaller.js
@@ -5,7 +5,7 @@ const DownloadWindowsInstaller = () => {
     const [url, setUrl] = useState('');
 
     useEffect(() => {
-        fetch('https://api.cloudrift.ai/internal/release/fair-desktop-full/stable/windows-x86_64/latest/url')
+        fetch('https://api.cloudrift.ai/internal/release/rift-desktop-full/stable/windows-x86_64/latest/url')
             .then((response) => response.text())
             .then((url) => setUrl(url));
     }, []);
